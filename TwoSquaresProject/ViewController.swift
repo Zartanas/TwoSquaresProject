596d9636ddc52fc5d194f19f5edf37dd85ccd0f5
Vladimir <dizzyrockin@mail.ru>
2024-01-24 15:56:06 +0100

    Initial Commit

---
+++ TwoSquaresProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoSquaresProject
+//
+//  Created by Владимир Румянцев on 24.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
